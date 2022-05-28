c98c618cb4acd379c66548823e39e0e51ad6eb9d
Emily Guidi <97910119+emyguidi07@users.noreply.github.com>
2022-05-28 20:09:30 -0300

    Update testelicao_test.js

--- testes/testelicao_test.js
+++ testes/testelicao_test.js
@@ -1,17 +1,18 @@
 import {
-    assertAlmostEquals,
-    assertExists
+  assertStrictEquals, 
+  assertAlmostEquals,
+  assertThrows,
+  assertNotEquals
 } from "http://deno.land/std/testing/asserts.ts" //arquivo de importações no Deno
-let num1 = 5
-let existir =" "
-Deno.test("Teste de assert almost", () => {
-    assertAlmostEquals(num1, "5"); //Make an assertion that actual and expected are almost equal numbers through a given tolerance
-})
-Deno.test("Teste de assert exists", () => {
-    if (existir === undefined || existir === null) {
-        if (!msg) {
-          msg = `actual: "${Existir}" expected to not be null or undefined`;
-        }
-        throw new AssertionError(msg);
-      }
-})
+Deno.test("Assert de elementos não estritamente iguais", () => {
+  const a = "3";
+  const b = "3";
+  assertStrictEquals(a, b);
+}); 
+Deno.test("Assert para números aproximados com throw", () => {
+  assertAlmostEquals(0.1 + 0.2, 0.3, 1e-16);
+  assertThrows(() => assertAlmostEquals(0.1 + 0.2, 0.3, 1e-17));
+});
+Deno.test("Assert de elementos que não são iguais", () => {
+  assertNotEquals(1, 2);
+});
